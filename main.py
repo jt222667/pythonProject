8123188761ab7ef0be3425f99f2934e5d040164b
jt <92149912@qq.com>
2025-11-05 22:36:16 +0800

    第二版

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ class SingleLinkArm:
         return x, y
 
 
-# 滑模控制器
-class SlidingModeController:
+# 滑模控制器（符号函数版）
+class SlidingModeControllerSign:
     def __init__(self, lambda_param, k, dt):
         self.lambda_param = lambda_param
         self.k = k
@@ -33,9 +33,8 @@ class SlidingModeController:
         self.dt = dt
 
     def control(self, angle, desired_angle):
-        angular_velocity = (angle - self.LAST_angular)/self.dt
-        desired_angular_velocity = (desired_angle - self.LAST_desired_angle)/self.dt
-
+        angular_velocity = (angle - self.LAST_angular) / self.dt
+        desired_angular_velocity = (desired_angle - self.LAST_desired_angle) / self.dt
 
         e = angle - desired_angle
         e_dot = angular_velocity - desired_angular_velocity
@@ -45,6 +44,31 @@ class SlidingModeController:
         self.LAST_angular = angle
         self.LAST_desired_angle = desired_angle
 
+        return u
+
+
+# 滑模控制器（饱和函数版）
+class SlidingModeControllerSaturation:
+    def __init__(self, lambda_param, k, epsilon, dt):
+        self.lambda_param = lambda_param
+        self.k = k
+        self.epsilon = epsilon
+        self.LAST_angular = 0.0
+        self.LAST_desired_angle = 0.0
+        self.dt = dt
+
+    def control(self, angle, desired_angle):
+        angular_velocity = (angle - self.LAST_angular) / self.dt
+        desired_angular_velocity = (desired_angle - self.LAST_desired_angle) / self.dt
+
+        e = angle - desired_angle
+        e_dot = angular_velocity - desired_angular_velocity
+        s = e_dot + self.lambda_param * e
+        # 使用饱和函数
+        u = -self.k * np.clip(s / self.epsilon, -1, 1)
+
+        self.LAST_angular = angle
+        self.LAST_desired_angle = desired_angle
 
         return u
 
@@ -60,41 +84,53 @@ frequency = 0.2
 desired_angle = amplitude * np.sin(2 * np.pi * frequency * time)
 desired_angular_velocity = 2 * np.pi * frequency * amplitude * np.cos(2 * np.pi * frequency * time)
 
-# 初始化控制器和机械臂
-arm = SingleLinkArm(length, dt)
-controller = SlidingModeController(lambda_param=10, k=5, dt=dt)
+# 初始化机械臂和控制器
+arm_sign = SingleLinkArm(length, dt)
+arm_saturation = SingleLinkArm(length, dt)
+
+controller_sign = SlidingModeControllerSign(lambda_param=10, k=5, dt=dt)
+controller_saturation = SlidingModeControllerSaturation(lambda_param=10, k=5, epsilon=0.1, dt=dt)
 
 # 存储结果
-angles = []
-errors = []
+angles_sign = []
+errors_sign = []
+angles_saturation = []
+errors_saturation = []
 
 # 仿真循环
 for t, angle_d, angular_velocity_d in zip(time, desired_angle, desired_angular_velocity):
-    # 计算控制输入
-    torque = controller.control(arm.angle,angle_d)
+    # 使用符号函数控制器
+    torque_sign = controller_sign.control(arm_sign.angle, angle_d)
+    arm_sign.update(torque_sign)
+    angles_sign.append(arm_sign.angle)
+    errors_sign.append(arm_sign.angle - angle_d)
 
-    # 更新机械臂状态
-    arm.update(torque)
-
-    # 存储结果
-    angles.append(arm.angle)
-    errors.append(arm.angle - angle_d)
+    # 使用饱和函数控制器
+    torque_saturation = controller_saturation.control(arm_saturation.angle, angle_d)
+    arm_saturation.update(torque_saturation)
+    angles_saturation.append(arm_saturation.angle)
+    errors_saturation.append(arm_saturation.angle - angle_d)
 
 # 绘制结果
 plt.figure(figsize=(12, 8))
 
+# 角度对比
 plt.subplot(2, 1, 1)
 plt.plot(time, desired_angle, 'r', label='Desired Angle')
-plt.plot(time, angles, 'b', label='Actual Angle')
+plt.plot(time, angles_sign, 'b', label='Actual Angle (Sign)')
+plt.plot(time, angles_saturation, 'g--', label='Actual Angle (Saturation)')
 plt.ylabel('Angle (rad)')
 plt.title('Sliding Mode Control of Single Link Arm')
 plt.legend()
 
+# 误差对比
 plt.subplot(2, 1, 2)
-plt.plot(time, errors, 'g')
+plt.plot(time, errors_sign, 'b', label='Tracking Error (Sign)')
+plt.plot(time, errors_saturation, 'g--', label='Tracking Error (Saturation)')
 plt.xlabel('Time (s)')
 plt.ylabel('Error (rad)')
-plt.title('Tracking Error')
+plt.title('Tracking Error Comparison')
+plt.legend()
 
 plt.tight_layout()
 plt.show()
