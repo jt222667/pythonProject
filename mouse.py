f51d58bf572c46fe5b7b676e868134952d2039fc
jt <92149912@qq.com>
2025-11-12 15:30:05 +0800

    1112

---
+++ mouse.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+import sys
+
+time.sleep(0.5)
+pyautogui.rightClick(2508, 1406)
+time.sleep(0.5)
+pyautogui.doubleClick(2474, 1298)
+time.sleep(0.5)
+pyautogui.doubleClick(1749, 600)
+time.sleep(0.5)
+
+pyautogui.doubleClick(2023, 294)
+time.sleep(0.5)
+pyautogui.doubleClick(2023, 453)
+time.sleep(0.5)
+pyautogui.doubleClick(2534, 20)
+
+time.sleep(0.5)
+pyautogui.rightClick(2508, 1406)
+time.sleep(0.5)
+pyautogui.doubleClick(2474, 1298)
+time.sleep(0.5)
+pyautogui.doubleClick(1749, 600)
+time.sleep(0.5)
+
+pyautogui.doubleClick(2015, 296)
+time.sleep(0.5)
+pyautogui.doubleClick(2015, 386)
+time.sleep(0.5)
+pyautogui.doubleClick(2534, 20)
+
+sys.exit()
